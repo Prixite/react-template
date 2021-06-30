fc1a60fe054f91d3d8090174857c8f20c53d4dd8
Umair Khan <umair.waheed@gmail.com>
2021-06-30 12:58:04 +0500

    Create App component

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Profile } from './components/Profile'
-import { UserList } from './components/UserList'
-import { CountryList } from './components/CountryList'
-
-function App () {
-  if (window.location.href.indexOf('/profile') >= 0) {
-    return <Profile />
-  } else if (window.location.href.indexOf('/users') >= 0) {
-    return <UserList />
-  } else if (window.location.href.indexOf('/countries') >= 0) {
-    return <CountryList />
-  } else {
-    return (
-      <ul>
-        <h1>React Template</h1>
-        <li><a href='/profile'>Profile</a></li>
-        <li><a href='/users'>Users</a></li>
-        <li><a href='/countries'>Countries</a></li>
-      </ul>
-    )
-  }
-}
+import App from './components/App'
 
 ReactDOM.render(
   <React.StrictMode>
